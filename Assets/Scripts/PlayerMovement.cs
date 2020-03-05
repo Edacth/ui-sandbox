106551af8fb935ff7ebe9cbb2bd475461e5f3305
Cade <cadeanderson99@gmail.com>
2020-03-05 11:14:23 -0800

    Player controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 movementVector;
+    
+
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        movementVector = Vector3.zero;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movementVector.y = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            movementVector.y = -1;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            movementVector.x = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            movementVector.x = -1;
+        }
+
+        if (true)
+        {
+            transform.position += movementVector;
+            
+        }
+        
+    }
+}
