57dd59c3b33b365579d3dda352b873ebe790b871
Cade <cadeanderson99@gmail.com>
2020-03-06 09:01:29 -0800

    Start on UI systems

---
+++ Assets/Scripts/IMoveable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMoveable
+{
+    bool selected { get; set; }
+
+    void Select();
+    void Deselect();
+
+    void Move();
+}
---
+++ Assets/Scripts/UIObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIObject : MonoBehaviour, IMoveable
+{
+    public bool selected { get; set; }
+    Vector3 movementVector;
+    float movementSpeed = 0.5f;
+
+    void Start()
+    {
+        Select();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Select()
+    {
+        selected = true;
+    }
+
+    public void Deselect()
+    {
+        selected = false;
+    }
+
+    public void Move()
+    {
+        movementVector = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            movementVector.y = movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movementVector.y = -movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movementVector.x = movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movementVector.x = -movementSpeed;
+        }
+
+        if (true)
+        {
+            transform.position += movementVector;
+
+        }
+    }
+}
